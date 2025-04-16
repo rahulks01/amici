61153cba071b7d087f98780df13eb52356312abf
Rahul Kumar Singh <ks.rahul0111@gmail.com>
2025-04-16 17:01:18 +0530

    JWT Token and Middleware

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -3,3 +3,4 @@ export const HOST = import.meta.env.VITE_BACKEND_URL;
 export const AUTH_ROUTE = "api/auth";
 export const SIGNUP_ROUTE = `${AUTH_ROUTE}/signup`;
 export const LOGIN_ROUTE = `${AUTH_ROUTE}/login`;
+export const GET_USER_INFO = `${AUTH_ROUTE}/user-info`;
